7585d691ff517b68a8910735efc4254fdfcf0841
Alex Barraza <38407344+sabarraz@users.noreply.github.com>
2022-08-11 23:10:50 -0700

    Updates with configuration parser
    Removed hardcode relative path and credentials

--- D42.py
+++ D42.py
@@ -8,59 +8,57 @@ from tkinter.font import names
 import requests
 import pandas as pd
 import urllib3
+import logging
+import configparser
+import glob, os
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+D42_username = config.get('Device42', 'user')
+D42_password = config.get('Device42', 'pass')
+D42_url = config.get('Device42', 'url')
+D42_TYPE = config.get('Device42', 'content-type')
+
 
 ##disables warning for unverified HTTPS (url)
 urllib3.disable_warnings()
 
-#url = 'https://10.201.10.198/api/1.0/devices/all/'
-url = 'https://swaggerdemo.device42.com/api/1.0/devices/all/'
-username = 'guest'
-password = 'device42_rocks!'
 headers = {
-            'Content-Type': 'application/json'
+            'Content-Type': D42_TYPE
         }
 
-r = requests.request("GET", url, auth=(username, password), headers=headers, verify=False)
-dictionary = json.loads(r.text)
 
-#---------------------------------------------------------------------------------------------
+r = requests.request("GET", D42_url, auth=(D42_username, D42_password), headers=headers, verify=False)
+dictionary = json.loads(r.text)
+#print(dictionary)
+#---------------------------------------------------------------------------------------------    
 ##Device name is 'Name'
-#if dictionary is Empty:
 deviceNames = []
 for e in range(len(dictionary.get('Devices'))):
     deviceNames.append(dictionary.get('Devices')[e]['name'])
-        
-#print(deviceNames)
-
 #---------------------------------------------------------------------------------------------
-## [NEEDS TESTING IN VERIZON]
-# Device location is 'Location' **SHOULD ONLY WORK IN VERIZON BOX NOT IN SANDBOX**
-#if dictionary is Empty:
-locations = []
+device_IDs = [[] for i in range(len(dictionary['Devices'][:]))]
 i = 0
 
 for e in dictionary['Devices'][:]:
-    if 'custom_fields' in e:
-        for x in e['custom_fields']:
-           if 'hdd_details' in x:
-               locations.append(dictionary.get('Devices')[x].get('location'))
-           elif 'location' not in x:
-                locations.append(None)
-    i=i+1
-'''
-for e in range(len(dictionary.get('Devices'))):
-    locations.append('')
-'''
-'''
-for e in range(len(dictionary.get('Devices'))):
-    if len(dictionary.get('Devices')[e]['location']) == 0:
-        locations.append('')
-    else:
-        locations.append(dictionary.get('Devices')[e]['location'])
-            
-    #print(locations)
-'''
+    if 'id' in e:
+        device_IDs[i].append(e['id'])
+    elif 'id' not in e:
+        device_IDs[i].append(None)
+    i=i+1  
+
+locations = [[] for i in range(len(dictionary['Devices'][:]))]
+i = 0
+
+for e in dictionary['Devices'][:]:
+    if 'location' in e:
+        locations[i].append("VZ Kearny, NJ")
+        #locations[i].append(e['location'])
+    elif 'location' not in e:
+        locations[i].append("VZ Kearny, NJ")
+    i=i+1  
 #---------------------------------------------------------------------------------------------
+
 ##Used For [Configuration Item] --> not in Verizon_Box, entered Manually ex.: 'Production' as the value
 ##BUT, it's probably 'service_level'
 #if dictionary is Empty:
@@ -71,9 +69,8 @@ for e in range(len(dictionary.get('Devices'))):
     else:
         service_levels.append(dictionary.get('Devices')[e]['service_level'])
 
-#print(service_levels)
-
 #---------------------------------------------------------------------------------------------
+
 ## [MISSING]? set to default 'Installed'
 ## Status
 ##Status in ServiceNow is ____ in Verizon_Box? I don't see it.
@@ -82,27 +79,25 @@ statuses = []
 #if dictionary is Empty:
 for e in range(len(dictionary.get('Devices'))):
     statuses.append('Installed')
-    #print(statuses)
+
 #---------------------------------------------------------------------------------------------
+
 ## [MISSING]? set to default 'Storage Device'
 ##Class in ServiceNow is ____ in Verizon_Box? IDK.
 #if dictionary is Empty:
 classes = []
 for e in range(len(dictionary.get('Devices'))):
     classes.append('Storage Device')
-'''
-    if dictionary is Empty:
-        for e in range(len(dictionary.get('Devices'))):
-            classes.append('Storage Device')
-        print(classes)
-'''
+
 #---------------------------------------------------------------------------------------------
 
 ## 'CC Type' not in Verizon_Box
 cc_types = []
 for e in range(len(dictionary.get('Devices'))):
     cc_types.append('')
+
 #---------------------------------------------------------------------------------------------
+
 ## [WORKING] NEED TO TEST
 ## 'Manufacturer' in ServicenNow is 'manufacturer' in Verizon_Box
 ##will not pull correctly, must be more nested...
@@ -111,27 +106,14 @@ manufacturer = [[] for i in range(len(dictionary['Devices'][:]))]
 i = 0
 
 for e in dictionary['Devices'][:]:
-    if 'custom_fields' in e:
-        for x in e['custom_fields']:
-           if 'manufacturer' in x:
-               manufacturer[i].append(x['manufacturer'])
-           elif 'manufacturer' not in x:
-                manufacturer[i].append(None)
+    if 'manufacturer' in e:
+        manufacturer[i].append(e['manufacturer'])
+    elif 'manufacturer' not in e:
+        manufacturer[i].append(None)
     i=i+1
 
-for e in range(len(dictionary.get('Devices'))):
-    manufacturer.append('')
-'''
-    for e in range(len(dictionary.get('Devices'))):
-        if dictionary.get('Devices')[e]['manufacturer'] is None:
-            manufacturer.append('')
-        else:
-            manufacturer.append(dictionary.get('Devices')[e]['manufacturer'])
-
-    print(manufacturer)
-'''
-
 #---------------------------------------------------------------------------------------------
+
 ## 'Model Number [Configuration Item]' in ServiceNow is Hardware Model 'hw_model' in Verizon_Box
 #if dictionary is Empty:
 model_numbers = []
@@ -142,23 +124,28 @@ for e in range(len(dictionary.get('Devices'))):
         model_numbers.append(dictionary.get('Devices')[e]['hw_model'])
 
 #---------------------------------------------------------------------------------------------
+
 ## [WORKING]
 ## 'IP Address [Configuration Item]' in ServiceNow is 'IP Address' in Verizon_Box GUI (not in the output.txt)
 ## using temporarily subnet or 'ip' since it's the closest to the IP Address, but missing part of it...
 #if dictionary is Empty:
-ip_addresses = [[] for i in range(len(dictionary['Devices'][:]))]
+ip_addr = [[] for i in range(len(dictionary['Devices'][:]))]
 i = 0
-print(len(ip_addresses))
-print(len(dictionary['Devices'][:]))
+
 for e in dictionary['Devices'][:]:
     if 'ip_addresses' in e:
         for x in e['ip_addresses']:
-           ip_addresses[i].append(x['ip'])
-           #if len(x) in e['ip_addresses'] == 0:
-           #    ip_addresses[i].append(None)
+            ip_addr[i].append(x['ip'])
     i=i+1
 
+ip_addresses = []
+for values in ip_addr:
+    if len(values) == 0:
+        ip_addresses.append("")
+    else:
+        ip_addresses.append(values[0])
 #---------------------------------------------------------------------------------------------
+
 ## Host Name [Configuration Item] in ServiceNow is 'virtual_host_name' in Verizon_Box (I think?)
 #if dictionary is Empty:
 virtual_host_names = []
@@ -167,9 +154,9 @@ for e in range(len(dictionary.get('Devices'))):
         virtual_host_names.append(None)
     else:
         virtual_host_names.append(dictionary.get('Devices')[e]['virtual_host_name'])
-#print(virtual_host_names)
 
 #---------------------------------------------------------------------------------------------
+
 ## Serial number in ServiceNow is 'serial_no' in Verizon_Box
 #if dictionary is Empty:
 serial_numbers = []
@@ -178,9 +165,9 @@ for e in range(len(dictionary.get('Devices'))):
         serial_numbers.append(None)
     else:
         serial_numbers.append(dictionary.get('Devices')[e]['serial_no'])
-#print(serial_numbers)
 
 #---------------------------------------------------------------------------------------------
+
 ## [WORKING]
 ## HWAddress (macaddress) belongs to 'MAC Address [Configuration Item]' in CSV file
 ##Needs to filter tuples, works currently for Empty and dictionaries.
@@ -191,42 +178,58 @@ i = 0
 for e in dictionary['Devices'][:]:
     if 'ip_addresses' in e:
         for x in e['ip_addresses']:
-           if 'macaddress' in x:
-               mac_addresses[i].append(x['macaddress'])
-           elif 'macaddress' not in x:
-                mac_addresses[i].append(None)
+            if 'macaddress' in x:
+                mac_addresses[i].append(x['macaddress'])
+            elif 'macaddress' not in x:
+                    mac_addresses[i].append(None)
     i=i+1
 
+mac_address = []
+for values in mac_addresses:
+    if len(values) == 0:
+        mac_address.append("")
+    else:
+        mac_address.append(values[0])
+'''
+for e in len(mac_addresses):
+    newLst.append(mac_addresses[e][0])
+
+mac_addresses = newLst
+'''
+
+        
+
 #---------------------------------------------------------------------------------------------
-## [NOT WORKING; NEEDS TESTING IN VERIZON]
+
+## [WORKING]
 ## 'Description [Configuration Item]' in ServiceNow is 'description' in Verizon_Box
 ## Needs to be coded in Verizon_Box since D42 sandbox is not giving proper output.
 ## Test in the box and parse hdd_details to get description or it might retrieve from just Devices than hdd_details
 #if dictionary is Empty:
-descriptions = []
-for e in range(len(dictionary.get('Devices'))):
-    descriptions.append('')
+descriptions = [[] for i in range(len(dictionary['Devices'][:]))]
+i = 0
 
-'''
-    #print(type(dictionary.get('Devices')[0]['hdd_details']))
-    for e in range(len(dictionary.get('Devices'))):
-        if dictionary.get('Devices')[e]['hdd_details'] is None:
-            descriptions.append(None)
-        else:
-            descriptions.append(dictionary.get('Devices')[e].get('hdd_details')) #Needs to retrieve description after 'hdd_details'
-    print(descriptions)
-'''
+for e in dictionary['Devices'][:]:
+    if 'description' in e:
+        descriptions[i].append(e['description'])
+    elif 'description' not in e:
+        descriptions[i].append(None)
+    i=i+1
 
 #---------------------------------------------------------------------------------------------
+
 ## Username [IGNORED, manually entered]
 usernames = []
 for e in range(len(dictionary.get('Devices'))):
     usernames.append('')
+
 #---------------------------------------------------------------------------------------------
+
 ## Password [IGNORED, manually entered]
 fs_passwords = []
 for e in range(len(dictionary.get('Devices'))):
     fs_passwords.append('')
+
 #---------------------------------------------------------------------------------------------
 
 ## 'Active Contract [Configuration Item]' in ServiceNow is not in Verizon_Box
@@ -237,72 +240,99 @@ active_contract = []
 for e in range(len(dictionary.get('Devices'))):
     active_contract.append(True)
     #print(len(active_contract))
+
 #---------------------------------------------------------------------------------------------
 
 ## [WORKING] Physical subtype (device_sub_type) in Verizon_Box is ____ in ServiceNow? Idk.
 ##D42 sandbox is called (virtual_subtype)
 #if dictionary is Empty:
-device_sub_type = []
+device_sub_type = [[] for i in range(len(dictionary['Devices'][:]))]
+i = 0
+
+for e in dictionary['Devices'][:]:
+    if 'device_sub_type' in e:
+        device_sub_type[i].append(e['device_sub_type'])
+    elif 'device_sub_type' not in e:
+        device_sub_type[i].append(None)
+    i=i+1
+
+#---------------------------------------------------------------------------------------------
+# 'rack' is location' but for now default and hardcode to 'Location'
+# which will then be read and replaced by a location that will be
+# posted to ServiceNow/D42
+rack_locations = []
 for e in range(len(dictionary.get('Devices'))):
-    if 'virtual_subtype' not in dictionary.get('Devices')[e]:
-        device_sub_type.append(None)
-    else:
-        device_sub_type.append(dictionary.get('Devices')[e]['virtual_subtype'])
+    rack_locations.append('Location')
 
-'''
-    for e in range(len(dictionary.get('Devices'))):
-        if 'virtual_subtype' not in dictionary.get('Devices')[e]:
-            device_sub_type.append(None) 
-            #or device_sub_type.append('')
-        else:
-            device_sub_type.append(dictionary.get('Devices')[e]['virtual_subtype'])
-
-    print(device_sub_type)
-'''
 #---------------------------------------------------------------------------------------------
 
-# 16/17 working properly (test in Verizon: description(unsure, test it), add(notes? *optional*), and manufacturer(working))
-CMDB_Items = ['Name', 
-              'Location', 
-              'Used for [Configuration Item]', 
-              'Status',                               #Set to 'Installed'
-              'Class',                                #Set to 'Storage Device'
-              'CC Type [Configuration Item]',         #IGNORED IN CODE
-              'Manufacturer', 
-              'Model number [Configuration Item]', 
-              'IP Address [Configuration Item',       
-              'Host Name [Configuration Item]', 
-              'Serial number', 
-              'MAC Address [Configuration Item]',     
-              'Description [Configuration Item]',     #In Verizon
-              'Username [Configuration Item]',        #IGNORED IN CODE
-              'FS Password [Configuration Item]',     #IGNORED IN CODE
-              'Active Contract [Configuration Item]', #Always 'True'
-              'Physical Subtype'                      #In Verizon
-              ]
+# 'customer'
+customers = [[] for i in range(len(dictionary['Devices'][:]))]
+i = 0
+
+for e in dictionary['Devices'][:]:
+    if 'customer' in e:
+        customers[i].append(e['customer'])
+    elif 'customer' not in e:
+        customers[i].append("")
+    i=i+1
+
+#---------------------------------------------------------------------------------------------
+
+discovery_source = []
+for e in range(len(dictionary.get('Devices'))):
+    discovery_source.append('Device42')
+
+#---------------------------------------------------------------------------------------------
 
 
+# [WORKING]
+CMDB_Items = [
+            'ID',
+            'Name', 
+            'Customer', 
+            'Location', 
+            'Used for [Configuration Item]', 
+            'Status',                               #Set to 'Installed'
+            'Class',                                #Set to 'Storage Device'
+            'CC Type [Configuration Item]',         #IGNORED
+            'Manufacturer', 
+            'Model number [Configuration Item]', 
+            'IP Address [Configuration Item',       
+            'Host Name [Configuration Item]', 
+            'Serial number', 
+            'MAC Address [Configuration Item]',     
+            'Description [Configuration Item]',     
+            'Username [Configuration Item]',        #IGNORED
+            'FS Password [Configuration Item]',     #IGNORED
+            'Active Contract [Configuration Item]', #Always 'True'
+            'Physical Subtype',
+            'Discovery Source'                      #Always 'Device42'
+            ]
 
-CMDB_Data = list(zip(deviceNames, 
-                     locations, 
-                     service_levels, 
-                     statuses, 
-                     classes,  
-                     cc_types, 
-                     manufacturer, 
-                     model_numbers, 
-                     ip_addresses, 
-                     virtual_host_names, 
-                     serial_numbers, 
-                     mac_addresses, 
-                     descriptions,
-                     usernames,
-                     fs_passwords,
-                     active_contract,
-                     device_sub_type
-                     ))
+CMDB_Data = list(zip(device_IDs,
+                    deviceNames, 
+                    customers,
+                    locations, 
+                    service_levels, 
+                    statuses, 
+                    classes,  
+                    cc_types, 
+                    manufacturer, 
+                    model_numbers, 
+                    ip_addresses, 
+                    virtual_host_names, 
+                    serial_numbers, 
+                    mac_address, 
+                    descriptions,
+                    usernames,
+                    fs_passwords,
+                    active_contract,
+                    device_sub_type,
+                    discovery_source
+                    ))
 
-with open('testD42_Data.csv', 'w', encoding='UTF8', newline='') as f:
+with open('Verizon_Data.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
     # write the headers
@@ -310,3 +340,53 @@ with open('testD42_Data.csv', 'w', encoding='UTF8', newline='') as f:
     # write the rows
     for data in CMDB_Data:
         writer.writerow(data)
+
+# Removes all unnecessary chars/symbols from csv file
+# reading the CSV file
+text = open("Verizon_Data.csv", "r")
+
+#join() method combines all contents of 
+# csvfile.csv and formed as a string
+text = ''.join([i for i in text]) 
+
+# search and replace the contents
+text = text.replace("[None]", "") 
+text = text.replace("[]", "") 
+text = text.replace("[", "") 
+text = text.replace("]", "") 
+text = text.replace(f"[\'", "")
+text = text.replace(f"\']", "")
+text = text.replace(f"\'", "")
+
+# testD42_Data.csv is the output file opened in write mode
+x = open("Verizon_Data.csv","w")
+
+# all the replaced text is written in the output.csv file
+x.writelines(text)
+x.close()
+
+##disables warning for unverified HTTPS (url)
+urllib3.disable_warnings()
+
+##Staging table URL 'u_d42_uploads'
+ 
+##credentials to SNOW
+SNOW_username = config.get('ServiceNow', 'user')
+SNOW_password = config.get('ServiceNow', 'pass')
+SNOW_url = config.get('ServiceNow', 'url')
+
+##Gets CSV file path
+PATH = os.path.dirname(os.path.abspath(__file__))
+os.chdir(PATH)
+for file in glob.glob("*.csv"):
+    directPath = PATH.replace("\\", "\\\\")
+    PATH = directPath + "\\\\" + file
+    break
+    
+##CSV file to upload
+csv_file = {
+    'import-file': open(f'{PATH}', 'rb')
+}
+
+##'POST' request to process CSV into the staging table
+r = requests.request("POST", SNOW_url, files=csv_file, auth=(SNOW_username, SNOW_password), verify=False)
